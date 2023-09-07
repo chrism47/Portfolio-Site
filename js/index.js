c0a63256130ba2cef4ffeb43c434f80378895cf6
chrism47 <chrismoulton.5369@gmail.com>
2023-09-07 04:58:46 -0700

    Templating and commenting done, ready for content and animations

---
+++ js/index.js
@@ -0,0 +1,118 @@
+//WARNING: the messiest part, it has all of the menu and menu animation logic
+
+const menuBtn = document.querySelector('.menu-container');
+const menuBtnText = document.querySelector('#menu-btn-text');
+
+const headerText = document.querySelector("#header-text-div");
+const headerBox = document.querySelector("#header-container");
+
+let exitBtnMove = document.createElement("h1");
+const fullHeadText = document.querySelector(".full-header").innerText;
+const smallHeadText = document.querySelector(".small-header").innerText;
+
+
+// This code adds an event listener to menuBtn, which, when clicked, dynamically 
+// a navigation menu with items from the "menuItems" array and appends it to the button, 
+// effectively replacing its original content, while also adding classes for styling to 
+// the button and the generated menu.------------------------------------>
+menuBtn.addEventListener("click", () => {
+    let menuItems = ["Home", "Services", "Projects", "Contact"]
+    let menuList = document.createElement("ul")//create ul out of array
+
+    menuItems.forEach(itemText => {
+        const menuItem = document.createElement("li");
+        
+        const anchor = document.createElement("a");
+        anchor.textContent = itemText;
+        anchor.classList.add("menu-text-style")
+        anchor.href = itemText.toLowerCase() + ".html";
+        if (itemText == "Home") {
+            anchor.href = "index.html"
+        }
+        anchor.addEventListener("click", (event) => {
+            event.stopPropagation();
+        })
+        
+        menuItem.textContent = "";
+        menuItem.appendChild(anchor);
+        menuList.appendChild(menuItem);
+
+
+    })//build list items and append
+
+    menuBtn.innerHTML = "";
+    menuBtn.appendChild(menuList);
+    //append the list, and remove original text
+
+    menuList.classList.add("ul-menu");
+    menuBtn.classList.add("menu-open");
+
+
+//----------This code handles the behavior of a mobile menu button and exit buttons 
+// in a web page's header. When the menuBtn has both the "menu-move" and "menu-open" classes, 
+// it adds an "X" exit button and defines its click behavior. Additionally, it updates the content and classes 
+// of various elements to accommodate the menu opening. When any element with the "exit-btn" class is clicked, 
+// it reverts the header and menu to their initial states.--------------------------------------------->
+    if (menuBtn.classList.contains("menu-move") && menuBtn.classList.contains("menu-open")) {
+        
+        exitBtnMove.innerText = "X";
+        exitBtnMove.classList.add("exit-btn-open")
+        menuBtn.insertAdjacentElement("afterend", exitBtnMove);
+        exitBtnMove.addEventListener("click", () => {
+            menuBtn.classList.remove("menu-open");
+            menuList.remove()
+            menuBtn.appendChild(menuBtnText)
+            document.querySelector(".full-header").innerText = fullHeadText
+            document.querySelector(".small-header").innerText = smallHeadText
+            document.querySelector(".full-header").classList.remove("exit-btn")
+            document.querySelector(".small-header").classList.remove("exit-btn")
+            exitBtnMove.remove()
+        });
+    };
+
+
+    document.querySelectorAll('.head').forEach((item) => {
+        item.innerHTML = "X";
+        item.classList.add("exit-btn");
+    }) 
+    //class changes to accomodate menu opening and heading removal
+    
+    document.querySelectorAll(".exit-btn").forEach((item) => {
+        item.addEventListener("click", () => {
+        
+        menuBtn.classList.remove("menu-open");
+        
+        menuList.remove();
+        if (document.contains(exitBtnMove)){
+        exitBtnMove.remove()};
+        menuBtn.appendChild(menuBtnText);
+        document.querySelector(".full-header").innerText = fullHead;
+        document.querySelector(".small-header").innerText = smallHead;
+        document.querySelector(".full-header").classList.remove("exit-btn");
+        document.querySelector(".small-header").classList.remove("exit-btn");
+    })//menu exiting behaviour, remove added classes and replace menu text
+    })
+});
+
+// observer for all animations, COMMENT WHEN DONE!!
+
+const options = {
+    root: null, // The viewport
+    rootMargin: '0px', // Margin around the root
+    threshold: .6, // When 99% of the element is visible
+  };
+  
+const observer = new IntersectionObserver(callback, options);
+function callback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            menuBtn.classList.remove("menu-move");
+            headerText.classList.remove("hide-text");
+        
+        } else {
+            menuBtn.classList.add("menu-move");
+            headerText.classList.add("hide-text");
+    }
+})};
+observer.observe(headerBox);
+
--- js/template.js
+++ js/template.js
@@ -1,114 +1,45 @@
-const menuBtn = document.querySelector('.menu-container');
-const menuBtnText = document.querySelector('#menu-btn-text');
-
-const headerText = document.querySelector("#header-text-div");
-const headerBox = document.querySelector("#header-container");
-
-let exitBtnMove = document.createElement("h1");
-
-menuBtn.addEventListener("click", () => {
-    let menuItems = ["Home", "Services", "Projects", "Contact"]
-    let menuList = document.createElement("ul")//create ul out of array
-
-    menuItems.forEach(itemText => {
-        const menuItem = document.createElement("li");
-        
-        const anchor = document.createElement("a");
-        anchor.textContent = itemText;
-        anchor.classList.add("menu-text-style")
-        anchor.href = itemText.toLowerCase() + ".html";
-        if (itemText == "Home") {
-            anchor.href = "index.html"
-        }
-        anchor.addEventListener("click", (event) => {
-            event.stopPropagation();
-        })
-        console.log(anchor.href)
-        menuItem.textContent = "";
-        menuItem.appendChild(anchor);
-        menuList.appendChild(menuItem);
+const head = document.querySelector("#header-templater");
+const foot = document.querySelector("#footer-templater");
 
 
-    })//build list items and append
+const urlHtmlArray = (window.location.href).split("/");
+const urlHtml = urlHtmlArray[urlHtmlArray.length - 1];
+let fullHead;
+let smallHead;
 
-    menuBtn.innerHTML = "";
-    menuBtn.appendChild(menuList);
-    //append the list, and remove original text
+switch(urlHtml) {
+    case "index.html":
+        fullHead = "Chris Moulton Dev";
+        smallHead = "CM Dev";
+        break;
+    case "contact.html":
+        fullHead = "Contact Me";
+        smallHead = "Contact";
+        break;
+    case "projects.html":
+        fullHead = "Projects";
+        smallHead = "Projects";
+        break;
+    case "services.html":
+        fullHead = "Services";
+        smallHead = "Services";
+        break;
+}
 
-    menuList.classList.add("ul-menu");
-    menuBtn.classList.add("menu-open");
+head.innerHTML = `
+<div id="header-container">
+        <div id="header-text-div">
+          <h1 class="full-header head">${fullHead}</h1>
+          <h1 class="small-header head">${smallHead}</h1>
+        </div>
+        <div class="menu-container">
+          <h3 id="menu-btn-text">More</h3>
+        </div>
+      </div>
+      `
 
-
-    if (menuBtn.classList.contains("menu-move") && menuBtn.classList.contains("menu-open")) {
-        
-        exitBtnMove.innerText = "X";
-        exitBtnMove.classList.add("exit-btn-open")
-        menuBtn.insertAdjacentElement("afterend", exitBtnMove);
-        console.log("open and moved")
-        exitBtnMove.addEventListener("click", () => {
-            menuBtn.classList.remove("menu-open");
-            menuList.remove()
-            menuBtn.appendChild(menuBtnText)
-            document.querySelector(".full-header").innerText = "Chris Moulton Dev"
-            document.querySelector(".small-header").innerText = "CM Dev"
-            document.querySelector(".full-header").classList.remove("exit-btn")
-            document.querySelector(".small-header").classList.remove("exit-btn")
-            exitBtnMove.remove()
-        });
-    };
-
-
-    document.querySelectorAll('.head').forEach((item) => {
-        item.innerHTML = "X";
-        item.classList.add("exit-btn");
-    }) 
-    //class changes to accomodate menu opening and heading removal
-    
-    document.querySelectorAll(".exit-btn").forEach((item) => {
-        item.addEventListener("click", () => {
-        
-        menuBtn.classList.remove("menu-open");
-        
-        menuList.remove()
-        if (document.contains(exitBtnMove)){
-        exitBtnMove.remove()}
-        menuBtn.appendChild(menuBtnText)
-        document.querySelector(".full-header").innerText = "Chris Moulton Dev"
-        document.querySelector(".small-header").innerText = "CM Dev"
-        document.querySelector(".full-header").classList.remove("exit-btn")
-        document.querySelector(".small-header").classList.remove("exit-btn")
-    })//menu exiting behaviour, remove added classes and replace menu text
-    })
-});
-
-const options = {
-    root: null, // The viewport
-    rootMargin: '0px', // Margin around the root
-    threshold: .6, // When 99% of the element is visible
-  };
-  
-const observer = new IntersectionObserver(callback, options);
-
-function callback(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            menuBtn.classList.remove("menu-move");
-            headerText.classList.remove("hide-text");
-            // if (menuBtn.classList.contains("menu-open")){
-            //     menuBtn.classList.remove("menu-open");
-            //     menuBtn.appendChild(menuBtnText)
-            // }
-        
-        } else {
-            menuBtn.classList.add("menu-move");
-            headerText.classList.add("hide-text");
-        //     if (menuBtn.classList.contains("menu-open")){
-            
-        // }
-    }
-})};
-
-
-
-observer.observe(headerBox);
-
+foot.innerHTML = `
+    <div id="footer-container">
+        <p>Here's my footer</p>
+    </div>
+    `
